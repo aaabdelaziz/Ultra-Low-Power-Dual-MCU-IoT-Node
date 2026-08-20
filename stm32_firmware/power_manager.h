16bc22b4896bb4c49bcb2cc65b5cdbeb627ce55e
Ahmed Abdelaziz <eng,.>
2026-08-20 12:03:55 +0200

    refactor: remove legacy STM32 firmware and add codebase documentation

--- stm32_firmware/power_manager.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-void power_manager_init(void);
-void enter_low_power_mode(void);
-#pragma once
-void pm_init(void);
-void pm_configure_unused_gpios_analog(void);
-void pm_enter_stop_mode(void);
--- stm32_firmware/uart_handshake.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stddef.h>
-void uart_handshake_send(const uint16_t* samples, size_t n);
-#pragma once
-#include "dma_adc.h"
-
-void uart_handshake_init(void);
-void uart_wake_esp32(void);
-void uart_send_frame_and_wait_ack(const telemetry_payload_t *payload);
